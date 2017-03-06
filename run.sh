0f1aa1eab2b586a5dc3ea0b77da3b01248d6f030
14kw <14codemail@gmail.com>
2017-03-06 17:37:20 +0900

    replace bucket url

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-aws s3 cp s3://web-aws-test/firebase-database-backup/serviceAccountKey.json /opt/docker-firebase-admin/serviceAccountKey.json
+aws s3 cp s3://${BUCKET}/serviceAccountKey.json /opt/docker-firebase-admin/serviceAccountKey.json
 cd /opt/docker-firebase-admin
 node app.js
